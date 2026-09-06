fc7a687856a2efead35dd467ef45eef8db6dbf9e
Jeremy Evans <code@jeremyevans.net>
2026-09-06 10:18:07 -0700

    Make JRUBY_VERSION check in test more understandable
    This only removed the methods on JRuby 9.4 and not JRuby 10, which
    is what is desired, but that happened by accident due to the
    string comparison. Make it clear that that is was is desired.

--- test/test_cicphash.rb
+++ test/test_cicphash.rb
@@ -26,7 +26,7 @@ class CICPHashTest < Minitest::Test
     cicphash_methods = CICPHash.public_instance_methods.sort
     hash_methods = Hash.public_instance_methods.sort
     assert_empty(cicphash_methods - hash_methods)
-    if defined?(JRUBY_VERSION) && JRUBY_VERSION >= '9.4'
+    if defined?(JRUBY_VERSION) && JRUBY_VERSION.to_r >= Rational('9.4') && JRUBY_VERSION.to_r < Rational(10)
       hash_methods -= [:index, :ruby2_keywords_hash, :ruby2_keywords_hash?]
     end
     assert_empty(hash_methods - cicphash_methods) 
